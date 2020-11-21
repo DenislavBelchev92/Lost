b39b6888cd2abfd086e67dbda61987235bc00b6e
Denis <denislav.belchev92@gmail.com>
2020-11-21 20:31:20 +0200

    Add login/register pages

--- src/apps/mainpage/views.py
+++ src/apps/mainpage/views.py
@@ -6,3 +6,17 @@ def index(request):
         'text': "Hi bitch",
     }
     return HttpResponse(render(request, 'mainpage/index.html', context))
+
+def login(request):
+    context = {
+        'login_page': "TRUE",
+
+    }
+    return HttpResponse(render(request, 'login/login.html', context))
+
+def register(request):
+    context = {
+        'register_page': "TRUE",
+    }
+
+    return HttpResponse(render(request, 'login/register.html', context))
