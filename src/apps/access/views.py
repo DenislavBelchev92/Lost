e6ae911d32a5d0000df678bc39c1a8f61ecee156
Denis <denislav.belchev92@gmail.com>
2020-12-10 22:35:33 +0200

    access module smal fix

--- src/apps/access/views.py
+++ src/apps/access/views.py
@@ -26,10 +26,16 @@ def register(request):
     if request.method == 'POST':
         form = RegisterForm(request.POST)
         if form.is_valid():
+            first_name = form.cleaned_data.get('first_name')
+            last_name = form.cleaned_data.get('last_name')
+            username = form.cleaned_data.get('username')
+            email = form.cleaned_data.get('email')
+            password = form.cleaned_data.get('password')
+
             context = {
-                'text': "loged_in",
+                'text': "You just registered with first_name " + first_name + " last_name " + last_name + " username " + " email " + email + " password " + str(password),
             }
-            return HttpResponse(render(request, 'access/register.html', context))
+            return HttpResponse(render(request, 'mainpage/index.html', context))
         else:
             context = {
                 'form' : form,
