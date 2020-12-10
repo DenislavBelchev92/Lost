3e540db7c51c18a63e1e2d75b637935fbc71230e
Denis <denislav.belchev92@gmail.com>
2020-12-10 23:38:53 +0200

    Complete access functionality

--- src/apps/access/forms.py
+++ src/apps/access/forms.py
@@ -6,11 +6,6 @@ class LoginForm(forms.Form):
     username = forms.CharField(label='Username', max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
     password = forms.CharField(label='Password', max_length=100, widget=forms.PasswordInput(attrs={'class': 'form-control '}))
 
-class RegisterForm_bak(LoginForm):
-    email = forms.EmailField(label='email', max_length=100, widget=forms.EmailInput(attrs={'class': 'form-control'}))
-    first_name = forms.CharField(label='First Name', max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
-    last_name = forms.CharField(label='Last Name', max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
-
 class RegisterForm(forms.ModelForm):
     class Meta:
         model = User
--- src/apps/access/views.py
+++ src/apps/access/views.py
@@ -1,16 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from .forms import LoginForm, RegisterForm
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
 
 def login(request):
     if request.method == 'POST':
         form = LoginForm(request.POST)
         if form.is_valid():
             username = form.cleaned_data.get('username')
-            context = {
-                'text': "loged_in " + username,
-            }
-            return HttpResponse(render(request, 'mainpage/index.html', context))
+            password = form.cleaned_data.get('password')
+
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                context = {
+                    'text': "loged_in " + username,
+                }
+                return HttpResponse(render(request, 'mainpage/index.html', context))
+
+            else:
+                context = {
+                    'form' : form,
+                    'text': 'Login failed',
+                }
+                return HttpResponse(render(request, 'access/login.html', context))
+
+                
         else:
             context = {
                 'form' : form,
@@ -26,11 +41,18 @@ def register(request):
     if request.method == 'POST':
         form = RegisterForm(request.POST)
         if form.is_valid():
+
             first_name = form.cleaned_data.get('first_name')
             last_name = form.cleaned_data.get('last_name')
             username = form.cleaned_data.get('username')
             email = form.cleaned_data.get('email')
             password = form.cleaned_data.get('password')
+
+            user = User.objects.create_user(username, email, password)
+            user.first_name = first_name
+            user.last_name = last_name
+
+            user.save()
 
             context = {
                 'text': "You just registered with first_name " + first_name + " last_name " + last_name + " username " + " email " + email + " password " + str(password),
