5489820e11be764045dfb7d50a77a7b3f7ddfea1
Denis <denislav.belchev92@gmail.com>
2020-12-12 19:39:02 +0200

    Start location module

--- src/apps/locations/models.py
+++ src/apps/locations/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Location(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=-1)
+    latitude = models.FloatField(default=0)
+    longitude = models.FloatField(default=0)
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
