8ae535b366e3680213d9b70571bed236579d9b19
Denis <denislav.belchev92@gmail.com>
2020-12-19 21:12:17 +0200

    Add location to admin and rearrange navbar

--- src/apps/locations/admin.py
+++ src/apps/locations/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Location
 
-# Register your models here.
+class LocationAdmin(admin.ModelAdmin):
+    # These are the fields when we add a location
+    # First is a topic so we don't need to specify options for it
+    fieldsets = [
+        (None,               {'fields': ['name']}),
+        ('Latitude',  {'fields': ['latitude']}),
+        ('Longitude',  {'fields': ['longitude']}),
+
+       # ('User ID',  {'fields': ['user_id']}),
+
+    ]
+
+    list_display = ('name', 'latitude', 'longitude', 'user_id')
+
+admin.site.register(Location, LocationAdmin)
